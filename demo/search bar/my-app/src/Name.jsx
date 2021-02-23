260d29ae273fbb573f226776f1dbb14699b4fe4a
noorbuchi <buchin@allegheny.edu>
2021-02-23 16:42:20 -0500

    add demo files without node_modules

---
+++ demo/search bar/my-app/src/Name.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Name extends Component {
+  render() {
+    return <div>{this.props.name}, style=font-family:"Comic Sans";</div>;
+  }
+}
+
+export default Name;
